4cb9ab06abe325f61fbb56394d1c6c8c431bb94d
rllyhz <rullyihza00@gmail.com>
2021-06-08 00:26:44 +0700

    Updating Register UI

--- app/src/main/java/com/example/angkoot/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/example/angkoot/ui/register/RegisterActivity.kt
@@ -1,24 +1,150 @@
 package com.example.angkoot.ui.register
 
 import android.os.Bundle
-import android.widget.Toast
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.angkoot.R
 import com.example.angkoot.databinding.ActivityRegisterBinding
+import com.example.angkoot.utils.EditTextInputUtils
+import com.example.angkoot.utils.ToastUtils
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    private val viewModel: RegisterViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupUI()
+        observeData()
+    }
+
+    private fun setupUI() {
         supportActionBar?.hide()
 
         with(binding) {
+            edtPhoneRegister.addTextChangedListener(phoneNumberTextWatcher)
+            edtUsernameRegister.addTextChangedListener(usernameTextWatcher)
+            edtPasswordRegister.addTextChangedListener(passwordTextWatcher)
+            edtConfirmPasswordRegister.addTextChangedListener(confirmPasswordTextWatcher)
+
             btnSignUp.setOnClickListener {
-                Toast.makeText(applicationContext, "Sign Up Success", Toast.LENGTH_SHORT).show()
+                ToastUtils.show(applicationContext, getString(R.string.register_success_message))
             }
+        }
+    }
+
+    private fun observeData() {
+        with(viewModel) {
+            with(binding) {
+                with(EditTextInputUtils) {
+
+                    phoneNumberText.observe(this@RegisterActivity) { phoneNumber ->
+                        if (!isPhoneNumberValid(phoneNumber))
+                            setError(
+                                edtPhoneRegister,
+                                getString(R.string.edt_phone_number_message)
+                            )
+                        else
+                            clearError(edtPhoneRegister)
+                    }
+
+                    usernameText.observe(this@RegisterActivity) { username ->
+                        if (!isUsernameValid(username))
+                            setError(
+                                edtUsernameRegister,
+                                getString(R.string.edt_username_message)
+                            )
+                        else
+                            clearError(edtUsernameRegister)
+                    }
+
+                    passwordText.observe(this@RegisterActivity) { password ->
+                        if (!isPasswordValid(password))
+                            setError(
+                                edtPasswordRegister,
+                                getString(R.string.edt_password_message)
+                            )
+                        else
+                            clearError(edtPasswordRegister)
+                    }
+
+                    confirmPasswordText.observe(this@RegisterActivity) { confirmPassword ->
+                        if (isPasswordValid(confirmPassword))
+                            setError(
+                                edtConfirmPasswordRegister,
+                                getString(R.string.edt_confirm_password_message)
+                            )
+                        else
+                            clearError(edtConfirmPasswordRegister)
+                    }
+                }
+
+                areAllInputsValid.observe(this@RegisterActivity) { valid ->
+                    btnSignUp.isActivated = valid
+                }
+            }
+        }
+    }
+
+    private var phoneNumberTextWatcher: TextWatcher? = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        override fun afterTextChanged(p0: Editable?) {}
+
+        override fun onTextChanged(tmpText: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            if (tmpText != null)
+                viewModel.setPhoneNumberText(tmpText.toString())
+        }
+    }
+
+    private var usernameTextWatcher: TextWatcher? = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        override fun afterTextChanged(p0: Editable?) {}
+
+        override fun onTextChanged(tmpText: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            if (tmpText != null)
+                viewModel.setUsernameText(tmpText.toString())
+        }
+    }
+
+    private var passwordTextWatcher: TextWatcher? = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        override fun afterTextChanged(p0: Editable?) {}
+
+        override fun onTextChanged(tmpText: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            if (tmpText != null)
+                viewModel.setPasswordText(tmpText.toString())
+        }
+    }
+
+    private var confirmPasswordTextWatcher: TextWatcher? = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        override fun afterTextChanged(p0: Editable?) {}
+
+        override fun onTextChanged(tmpText: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            if (tmpText != null)
+                viewModel.setConfirmPasswordText(tmpText.toString())
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        with(binding) {
+            edtPhoneRegister.removeTextChangedListener(phoneNumberTextWatcher)
+            phoneNumberTextWatcher = null
+            edtUsernameRegister.removeTextChangedListener(usernameTextWatcher)
+            usernameTextWatcher = null
+            edtPasswordRegister.removeTextChangedListener(passwordTextWatcher)
+            passwordTextWatcher = null
+            edtConfirmPasswordRegister.removeTextChangedListener(confirmPasswordTextWatcher)
+            confirmPasswordTextWatcher = null
         }
     }
 }
